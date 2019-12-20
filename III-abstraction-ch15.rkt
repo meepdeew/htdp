4900f9c87b2247bbc8faac31507be7325683465a
Rex Suter <rex.suter@brightmachines.com>
2019-12-19 16:32:08 -0800

    stash up to Exercise 253

--- III-abstraction-ch15.rkt
+++ III-abstraction-ch15.rkt
@@ -25,6 +25,10 @@
 (define (C2F c)
   (+ (* 9/5 c) 32))
 
+(check-expect (cf* '(10 20 30))
+              (list (C2F 10) (C2F 20) (C2F 30)))
+
+; ]List-of IR] -> List-of-strings
 ; Inventory -> List-of-strings
 ; extracts the names of
 ; toys from an inventory
@@ -44,6 +48,11 @@
 ; An Inventory is one of:
 ; - '()
 ; - (cons IR Inventory)
+
+;; Q: Why define Inventory rather than just saying [List-of IR]
+
+(check-expect (names (list (make-IR "yo" 32) (make-IR "dude" 43)))
+              (list "yo" "dude"))
 
 
 (define (map1 k g)
@@ -77,6 +86,9 @@
                 (make-IR "bear" 13.0)))
               (list "doll" "bear"))
 
+(check-expect (add-1-to-each '(1 2 3 4))
+              '(2 3 4 5))
+
 ; List-of-numbers -> List-of-numbers
 (define (add-1-to-each l)
   (map1 l add1))
@@ -96,12 +108,12 @@
 ; and 0 (incl.) in a list
 (define (tab-sqrt n)
   (cond [(= n 0) (list (sqrt 0))]
-        [else (cons (sqrt n) (sub1 n))]))
+        [else (cons (sqrt n) (tab-sqrt (sub1 n)))]))
 
 
 (define (tabulate n cb)
   (cond [(= n 0) (list (cb 0))]
-        [else (cons (cb n) (sub1 n))]))
+        [else (cons (cb n) (tabulate (sub1 n) cb))]))
 
 ; Number -> [List-of Number]
 ; tabulates sqrt between n
@@ -114,6 +126,12 @@
 ; and 0 (incl.) in a list
 (define (tab-sin* n)
   (tabulate n sin))
+
+(check-expect (tab-sqr 4)
+              (list (sqr 4) (sqr 3) (sqr 2) (sqr 1) (sqr 0)))
+
+(define (tab-sqr n)
+  (tabulate n sqr))
 
 (define (tab-tan n)
   (tabulate n tan))
@@ -143,15 +161,22 @@
               24)
 
 ; [List-of Any] fn Any -> Any
-(define (fold1 l cb r4bc)
-  (cond [(empty? l) r4bc]
+(define (fold1 l cb base-case)
+  (cond [(empty? l) base-case]
         [else (cb (first l)
-                  (fold1 (rest l) cb r4bc))]))
+                  (fold1 (rest l) cb base-case))]))
 
 (check-expect (fold1 '(1 2 3 4) + 0)
               10)
+
+(check-expect (fold1 '(1 2 3 4) + 0)
+              (+ 1 (+ 2 (+ 3 (+ 4 0)))))
+
 (check-expect (fold1 '(1 2 3 4) * 1)
               24)
+
+(check-expect (fold1 '(1 2 3 4) * 1)
+              (* 1 (* 2 (* 3 (* 4 1)))))
 
 ;; Exercise 252
 
@@ -181,6 +206,7 @@
         [else (place-dot (first l)
                          (image* (rest l)))]))
 
+; Q: Supposed to be the same implemetation as 251's fold1?
 (define (fold2 l cb base-case)
   (cond [(empty? l) base-case]
         [else (cb (first l)
@@ -193,13 +219,144 @@
                      * 1)
               (product '(2 3 4 5)))
 
+(check-expect (fold2 '(2 3 4 5) * 1)
+              (* 2 (* 3 (* 4 (* 5 1)))))
+
 (check-expect (fold2 (list (make-posn 2 1)
                            (make-posn 3 4))
                      place-dot emt)
               (image* (list (make-posn 2 1)
                             (make-posn 3 4))))
 
+(check-expect (fold2 (list (make-posn 2 1) (make-posn 3 4)) place-dot emt)
+              (place-dot (make-posn 2 1) (place-dot (make-posn 3 4) emt)))
+
 ;; 15.2 Similarities in Signatures
+
+
+; Number Boolean -> String
+(define (f n b) "hello world")
+
+; A parametrized signature, such as:
+; [X Y] [List-of X] -> [List-of Y]
+
+; can  be instantiated as (for `cf*`):
+; [List-of Number] -> [List-of Number]
+
+; or as (for `names`):
+; [List-of IR] -> [List-of String]
+
+
+
+; We never declared a signature for `map1`
+
+; Here are the signatures of two functions we wrote
+; that make use of abstract function `map1`:
+
+; List-of-numbers -> List-of-numbers
+;(define (cf*-from-map1 l) (map1 l C2F))
+; and C2F was: Number -> Number
+
+; Inventory -> List-of-strings
+;(define (names-from-map1 i) (map1 i IR-name))
+; and IR-name was: IR -> String
+
+
+; [X Y] [List-of X] [X -> Y] -> [List-of Y]
+
+
+; [List-of Number] -> Number
+; [List-of Posn] -> Image
+
+; [List-of Number] Number [Number Number -> Number]
+; -> Number
+(define (pr* l bs jn)
+  (cond
+    [(empty? l) bs]
+    [else
+     (jn (first l)
+         (pr* (rest l)
+              bs
+              jn))]))
+
+; [List-of Posn] Image [Posn Image -> Image]
+; -> Image
+(define (im* l bs jn)
+  (cond
+    [(empty? l) bs]
+    [else
+     (jn (first l)
+         (im* (rest l)
+              bs
+              jn))]))
+
+; [X Y] [List-of X] Y [X Y -> Y] -> Y
+; (define fold2 (...) ...)
+; Although mine had base-case and callback order flipped.
+
+; Exercise 253
+
+; [Number -> Boolean]
+(check-expect (even? 4) #true)
+(check-expect (even? 1) #false)
+(check-error (even? 'foo))
+
+; [Boolean String -> Boolean]
+(check-expect (boolean=?))
+
+
+; [Number Number Number -> Number]
+(check-expect (+ 1 2 3) 6)
+(check-error (+ 1 2 'foo))
+
+; [Number -> [List-of Number]]
+
+; [[List-of Number] -> Boolean]
+
+
+; Exercise 254
+
+(check-expect (sort-n '(2 7 1 4) <) '(1 2 4 7))
+
+;; [Listof Number [Number Number -> Boolean] -> Listof Number]
+(define (sort-n lon cb)
+  ())
+
+(check-expect)
+
+;; [Listof String [String String -> Boolean] -> Listof String]
+(define (sort-s los cb)
+  ())
+
+;; [Listof IR [IR IR -> Boolean] -> Listof IR]
+(define (sort-ir loir cb)
+  ())
+
+;; [Listof X [X X -> Boolean] -> Listof X]
+(define (sort-generic listo cb)
+  ())
+
+; Exercise 255
+
+
+
+
+
+
+
+
+
+;; C-(		paredit-backward-slurp-sexp
+;; C-)		paredit-forward-slurp-sexp
+
+;; C-{		paredit-backward-barf-sexp
+;; C-}		paredit-forward-barf-sexp
+
+;; <C-M-left>	paredit-backward-slurp-sexp
+;; <C-M-right>	paredit-backward-barf-sexp
+;; <C-left>	paredit-forward-barf-sexp
+;; <C-right>	paredit-forward-slurp-sexp
+
 
 "end"
 
