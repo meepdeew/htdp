4be6c1cdd58847ee8c1b83b41871979d55c48691
meepdeew <meepdeew.github@gmail.com>
2019-12-31 15:48:17 -0800

    finish 16.7 finger exercises: abstraction

--- III-abstraction-ch16.rkt
+++ III-abstraction-ch16.rkt
@@ -559,7 +559,387 @@
 
 ;;;; 16.7 Finger Exercises: Abstraction
 
+;; Exercise 267.
+
+(define CONVERSION 1.06)
+
+(check-expect (convert-euro (list 1.06 2.12))
+              (list 1.00 2.00))
+
+; [Listof Number] -> [Listof Number]
+; Converts a list of US$ amounts into a list of € amounts
+; based on an exchange rate of US$1.06 per € (on April 13, 2017).
+(define (convert-euro lon)
+  (local (; Number -> Number
+          ; convert a single euro amount to usd
+          (define (to-dollars euro)
+            (* euro (/ 1 CONVERSION))))
+    (map to-dollars lon)))
+
+(check-expect (convertFC (list 32 212))
+              (list 0 100))
+
+; [Listof Number] -> [Listof Number]
+; converts a list of Fahrenheit measurements to a list of Celsius measurements.
+(define (convertFC lon)
+  (local (; Number -> Number
+          ; convert a single fahrenheit measure to celsius
+          (define (f->c f)
+            (* (- f 32) (/ 5 9))))
+    (map f->c lon)))
+
+(check-expect (translate (list (make-posn 1 2) (make-posn 3 4)))
+              (list (list 1 2) (list 3 4)))
+
+; [Listof Posn] -> [Listof [Listof Number]]
+; translates a list of Posns into a list of lists of pairs of numbers.
+(define (translate lop)
+  (local (; Posn -> [Listof Number]
+          ; convert a Posn structure to a list
+          (define (posn->pair p)
+            (list (posn-x p) (posn-y p))))
+    (map posn->pair lop)))
+
+;; Exercise 268
+
+; Inventory
+(define-struct inventory
+  [item-name
+   item-description
+   acquisition-price
+   sales-price])
+
+(check-expect (inv-sort (list (make-inventory "1" "1" 4 2)
+                              (make-inventory "2" "2" 4 3)
+                              (make-inventory "3" "3" 4 4)))
+              (list (make-inventory "3" "3" 4 4)
+                    (make-inventory "2" "2" 4 3)
+                    (make-inventory "1" "1" 4 2)))
+
+; [Listof Inventory] -> [Listof Inventory]
+; sorts a list of inventory records by the difference between the two prices. 
+(define (inv-sort loi)
+  (local (; Inventory Inventory -> Boolean
+          ; whether the difference in the two prices for a given
+          ; inventory is less for inv1 than for inv2
+          (define (inv-cmp inv1 inv2)
+            (< (- (inventory-acquisition-price inv1) (inventory-sales-price inv1))
+               (- (inventory-acquisition-price inv2) (inventory-sales-price inv2)))))
+    (sort loi inv-cmp)))
+
+;; Exercise 269
+
+(check-expect (eliminate-expensive 4 (list (make-inventory "1" "1" 4 2)
+                                           (make-inventory "2" "2" 4 3)
+                                           (make-inventory "3" "3" 4 4)))
+              (list (make-inventory "1" "1" 4 2)
+                    (make-inventory "2" "2" 4 3)))
+
+; Number [Listof Inventory] -> [Listof Inventory]
+; produce a list of Inventory whose sales price is below ua
+(define (eliminate-expensive ua loi)
+  (local (; Inventory -> Boolean
+          (define (not-expensive i)
+            (< (inventory-sales-price i) ua)))
+    (filter not-expensive loi)))
+
+(check-expect (recall "namehere" (list (make-inventory "namehere" "1" 4 2)
+                                       (make-inventory "howdy" "2" 4 3)
+                                       (make-inventory "errbody" "3" 4 4)))
+              (list (make-inventory "howdy" "2" 4 3)
+                    (make-inventory "errbody" "3" 4 4)))
+
+; String [Listof Inventory] -> [Listof Inventory]
+; produce a list of Inventory records that do not use the name ty
+(define (recall ty loi)
+  (local (; Inventory -> Boolean
+          ; is record not matching the name ty
+          (define (not-of-name-ty i)
+            (not (string=? ty (inventory-item-name i)))))
+    (filter not-of-name-ty loi)))
+
+(check-expect
+ (selection (list "a" "d" "f") (list "b" "c" "d" "e" "f"))
+ (list "d" "f"))
+
+; [Listof String] [Listof String] -> [Listof String]
+; select all those from the second one that are also on the first
+(define (selection lon1 lon2)
+  (local (; [Listof String] String -> Boolean
+          (define (in-list? pseudo-lon item)
+            (cond [(empty? pseudo-lon) #false]
+                  [(string=? item (first pseudo-lon)) #true]
+                  [else (in-list? (rest pseudo-lon) item)]))
+          ; String -> Boolean
+          (define (exist-in-lon1 lon-2-item)
+            (in-list? lon1 lon-2-item)))
+    (filter exist-in-lon1 lon2)))
+
+;; Exercise 270
+
+(check-expect
+ (zero-thru-n-1 5)
+ (list 0 1 2 3 4))
+
+; Number -> [Listof Number]
+; creates the list (list 0 ... (- n 1)) for any natural number n;
+(define (zero-thru-n-1 n)
+  (local (; Number -> Number
+          ; return the input
+          (define (identity-fn n) n))
+    (build-list n identity-fn)))
+
+(check-expect (one-thru-n 5) (list 1 2 3 4 5))
+
+; Number -> [Listof Number]
+; creates the list (list 1 ... n) for any natural number n;
+(define (one-thru-n n)
+  (build-list n add1))
+
+(check-expect (build-frac 5)
+              (list 1 1/2 1/3 1/4 1/5))
+
+; Number -> [Listof (fractional) Number]
+; creates the list (list 1 1/2 ... 1/n) for any natural number n;
+(define (build-frac n)
+  (local (; Number -> (Fractional) Number
+          ; return the reciprocal of the input
+          (define (inverter n)
+            (/ 1 (add1 n))))
+    (build-list n inverter)))
+
+(check-expect (build-evens 4)
+              (list 0 2 4 6))
+
+; Number -> [Listof Number]
+; creates the list of the first n even numbers
+(define (build-evens n)
+  (local ((define (doubler x)
+            (* 2 x)))
+    (build-list n doubler)))
+
+(check-expect
+ (identityM2 1)
+ (list (list 1)))
+
+(check-expect
+ (identityM2 3)
+ (list (list 1 0 0) (list 0 1 0) (list 0 0 1)))
+
+; Number -> [Listof [Listof Number]]
+; creates a diagonal square of 0s and 1s; see exercise 262.
+(define (identityM2 n)
+  (local (; Number -> [Listof Number]
+          ; return sublist fully populated
+          (define (build-sublist current)
+            (local (; Number -> Number
+                    ; return value for sublist
+                    (define (populate-sublist-from-current i)
+                      (if (= i current) 1 0)))
+              (build-list n populate-sublist-from-current))))
+    (build-list n build-sublist)))
+
+; define tabulate from exercise 250 using build-list.
+
+; Number -> Number
+(define (my-identity2 i) i)
+
+(check-expect (tabulate 4 my-identity2)
+              (list 0 1 2 3))
+
+; Number [Number -> Number] -> [Listof Number]
+; tabulates cb res from 0..n-1 in a list
+(define (tabulate n cb)
+  (build-list n cb))
+
+;; Exercise 271
+
+(check-expect (find-name "joe" (list "joe" "phil")) #true)
+(check-expect (find-name "joe" (list "joey" "phil")) #true)
+(check-expect (find-name "joseph" (list "joey" "phil")) #false)
+
+; String [Listof String] -> Boolean
+; determines whether any of the names are equal to or an extension of the name.
+(define (find-name name lon)
+  (local ((define (name=? x)
+            (string-contains? name x)))
+    (ormap name=? lon)))
+
+(check-expect (starts-with-a? (list "joe" "armada")) #false)
+(check-expect (starts-with-a? (list "ayaz" "armada")) #true)
+
+; [Listof String] -> Boolean
+(define (starts-with-a? lon)
+  (local ((define (individual-holds? x)
+            (string=? "a" (first (explode x)))))
+    (andmap individual-holds? lon)))
+
+;; Should you use ormap or andmap to define a function that
+;; ensures that no name on some list exceeds a given width? 
+;; -> Both are viable with logical negation
+
+;; Exercise 272
+
+(check-expect
+ (equal? (append (list 1 2 3) (list 4 5 6 7 8))
+         (list 1 2 3 4 5 6 7 8))
+ #true)
+
+(check-expect (append-from-fold (list 1 2 3) (list 4 5 6 7 8))
+              (list 1 2 3 4 5 6 7 8))
+
+(check-expect (append-from-fold (list) (list 1 2 3)) '(1 2 3))
+(check-expect (append-from-fold (list 1 2 3) (list)) '(1 2 3))
+
+;; TODO How get variable number args and recursively call append-from-fold for n lists?
+(define (append-from-fold l1 l2)
+  (foldr cons l2 l1))
+
+;; What happens if you replace foldr with foldl?
+;; -> Flips the order in which l2 is cons'ed onto l1,
+;;    but not the order of l1 itself (base case is already cons'ed together).
+
+(check-expect (fold-sum (list 1 2 3 4)) 10)
+
+; [Listof Number] -> Number
+(define (fold-sum lon)
+  (foldr + 0 lon))
+
+(check-expect (fold-product (list 1 2 3 4)) 24)
+; [Listof Number] -> Number
+(define (fold-product lon)
+  (foldr * 1 lon))
+
+(define RECT (rectangle 10 10 "solid" "red"))
+
+(check-expect (horizontal-image-compose (list RECT RECT RECT))
+              (beside RECT (beside RECT (beside RECT empty-image))))
+
+(define (horizontal-image-compose loi)
+  (foldr beside empty-image loi))
+
+(check-expect (vertical-image-compose (list RECT RECT RECT))
+              (above RECT (above RECT (above RECT empty-image))))
+
+(define (vertical-image-compose loi)
+  (foldr above empty-image loi))
+
+;; foldr vs. foldl
+
+(define RECT1 (rectangle 10 10 "solid" "red"))
+(define RECT2 (rectangle 20 20 "solid" "orange"))
+(define RECT3 (rectangle 30 30 "solid" "yellow"))
+
+(check-expect (foldr beside empty-image (list RECT1 RECT2 RECT3))
+              (beside RECT1 (beside RECT2 (beside RECT3 empty-image))))
+
+(check-expect (foldl beside empty-image (list RECT1 RECT2 RECT3))
+              (beside RECT3 (beside RECT2 (beside RECT1 empty-image))))
+
+(check-expect (foldr above empty-image (list RECT1 RECT2 RECT3))
+              (above RECT1 (above RECT2 (above RECT3 empty-image))))
+
+(check-expect (foldl above empty-image (list RECT1 RECT2 RECT3))
+              (above RECT3 (above RECT2 (above RECT1 empty-image))))
+
+;; Exercise 273
+
+;; foldr signature
+; [X Y -> Y] Y [Listof X] -> Y
+
+;; map signature
+; [X -> Y] [Listof X] -> [Listof Y]
+
+(check-expect
+ (map-from-fold add1 (list 3 4 5))
+ (list 4 5 6))
+
+(check-expect
+ (map-from-fold add1 (list 3 4 5))
+ (cons (add1 3)
+       (cons (add1 4)
+             (cons (add1 5) '()))))
+
+; [X -> Y] [Listof X] -> [Listof Y]
+(define (map-from-fold cb lo-any)
+  (local (; X Y -> Y
+          (define (cons-and-call item prev)
+            (cons (cb item) prev)))
+    (foldr cons-and-call '() lo-any)))
+
+;; Exercise 274
+
+(check-expect (prefixes (list "a" "b" "c" "d"))
+              (list (list "a" "b" "c" "d")
+                    (list "a" "b" "c")
+                    (list "a" "b")
+                    (list "a")
+                    empty))
+(check-expect (prefixes (list "a" "b"))
+              (list (list "a" "b")
+                    (list "a")
+                    empty))
+(check-expect (prefixes (list "a"))
+              (list (list "a")
+                    empty))
+(check-expect (prefixes (cons "a" empty))
+              (cons (cons "a" empty)
+                    (cons empty '())))
+(check-expect (prefixes empty) (list empty))
+
+; [Listof 1String] -> [Listof [Listof 1String]]
+; consumes a list of 1Strings and produce a list of all prefixes
+(define (prefixes lo1s)
+  (local (; 1String [Listof 1String] -> [Listof 1String]
+          (define (prefix-sublist item listo)
+            (cond [(empty? listo) '()]
+                  [(string=? item (first listo)) listo]
+                  [else (prefix-sublist item (rest listo))]))
+          ; 1String [Listof [Listof 1String]] -> [Listof [Listof 1String]]
+          (define (prefix-helper item prev)
+            (cons (reverse (prefix-sublist item (reverse lo1s))) prev)))
+    (foldl prefix-helper (list empty) lo1s)))
+
+(check-expect (suffixes (list "a" "b" "c" "d"))
+              (list (list "a" "b" "c" "d")
+                    (list     "b" "c" "d")
+                    (list         "c" "d")
+                    (list             "d")
+                    empty))
+(check-expect (suffixes (list "a" "b"))
+              (list (list "a" "b")
+                    (list     "b")
+                    empty))
+(check-expect (suffixes (list "a"))
+              (list (list "a")
+                    empty))
+(check-expect (suffixes (cons "a" empty))
+              (cons (cons "a" empty)
+                    (cons empty '())))
+(check-expect (suffixes empty) (list empty))
+
+; [Listof 1String] -> [Listof [Listof 1String]]
+; consumes a list of 1Strings and produce a list of all suffixes
+(define (suffixes lo1s)
+  (local (; 1String [Listof 1String] -> [Listof 1String]
+          (define (suffix-sublist item listo)
+            (cond [(empty? listo) '()]
+                  [(string=? item (first listo)) listo]
+                  [else (suffix-sublist item (rest listo))]))
+          ; 1String [Listof [Listof 1String]] -> [Listof [Listof 1String]]
+          (define (suffix-helper item prev)
+            (cons (suffix-sublist item lo1s) prev)))
+    (foldr suffix-helper (list empty) lo1s)))
+
 ;;;; 16.8 Projects: Abstraction
+
+;; Exercise 275
+
+;; Exercise 276
+
+;; Exercise 277
+
+;; Exercise 278
 
 "end"
 
