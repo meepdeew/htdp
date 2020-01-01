d2cb65dd064f2e1b745aed4ab969e6857faf014d
meepdeew <meepdeew.github@gmail.com>
2020-01-01 12:47:00 -0800

    Begin ch. 17 Namesless Functions

---
+++ III-abstraction-ch17.rkt
@@ -0,0 +1,26 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname III-abstraction-ch16) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require test-engine/racket-tests)
+
+;;;; 17 Namesless Functions
+
+(require 2htdp/image)
+(require 2htdp/universe)
+
+
+;;;; 17.1 Functions from lambda
+
+;; Exercise 279
+
+;;;; 17.2 Computing with lambda
+
+;;;; 17.3 Abstracting with lambda
+
+;;;; 17.4 Specifying with lambda
+
+;;;; 17.5 Representing with lambda
+
+"end"
+
+(test);; DrRacket does something like this behind the scenes
